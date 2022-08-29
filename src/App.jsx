915ba1fcc88ec69814c76a59f37e0a4c6b209b52
4SAMU <samuelnzomo4@gmail.com>
2022-08-29 15:47:11 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import LoadingBar from 'react-top-loading-bar';
 
 import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
@@ -9,7 +10,15 @@ const API_URL = "https://www.omdbapi.com?apikey=b6003d8a";
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
+  const [progress, setProgress]=useState(0);
 
+  const progressBar=()=>{
+    setProgress(progress+100)
+    if(progress===100)
+    setProgress(0);
+  }
+
+ 
   useEffect(() => {
     searchMovies("");
   }, []);
@@ -23,6 +32,15 @@ const App = () => {
 
   return (
     <div className="app">
+
+  <div>
+    <LoadingBar
+    color='#f11946'
+    progress={progress}
+    onLoaderFinished={() => setProgress(0)}
+  />
+    
+    </div>
       <h1>SAMUTECH studio</h1>
 
       <div className="search">
@@ -30,12 +48,12 @@ const App = () => {
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Search for movies"
-          onKeyPress={(e) => e.key === 'Enter' && searchMovies(searchTerm)}
+          onKeyPress={(e) => e.key === 'Enter'&&progressBar()&& searchMovies(searchTerm)}
         />
         <img
           src={SearchIcon}
           alt="search"
-          onClick={() => searchMovies(searchTerm)}      
+          onClick={() => progressBar()&&searchMovies(searchTerm)}      
 
         />
       </div>
